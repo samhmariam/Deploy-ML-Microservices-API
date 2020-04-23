9c6290d4b7051cf20769af67ebf902be9b36a1e8
samhmariam <samhmariam@gmail.com>
2020-04-23 21:45:23 +0100

    Updated to upload_docker.sh.

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+# dockerpath=<samhmariam/path>
+dockerpath=samhmariam/app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag app samhmariam/udacity-microservices-proj:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push samhmariam/udacity-microservices-proj:latest
