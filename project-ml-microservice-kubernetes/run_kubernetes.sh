acf6bab656252c563893183ac3a8cbda6e2e82f2
samhmariam <samhmariam@gmail.com>
2020-04-24 12:48:15 +0100

    update to run_kubernetes.sh file

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -9,9 +9,10 @@ dockerpath="samhmariam/udacity-microservices-proj"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run microproj\
-    --image=$dockerpath\
-    --port=80 --labels app=microproj
+# kubectl run microproj\
+#    --image=$dockerpath\
+#    --port=80 --labels app=microproj
+kubectl create deployment microproj --image=samhmariam/udacity-microservices-proj
 
 # Step 3:
 # List kubernetes pods
@@ -19,5 +20,7 @@ kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward microproj 8000:80
+# kubectl port-forward microproj 8000:80
+kubectl expose deployment microproj --port=80 --type=NodePort
+kubectl port-forward deployment/microproj 8000:80
 
